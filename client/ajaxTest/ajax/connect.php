ceedf1d3dfc85cb49bf2eb77ebabeafe7cf11910
Jaan Taponen <jaan.taponen@hotmail.com>
2019-04-25 13:38:28 +0300

    dokumentaatio

--- client/ajaxTest/ajax/connect.php
+++ client/ajaxTest/ajax/connect.php
@@ -1,4 +1,7 @@
 <?php
+/*Tähän tiedostoon liitetään database kirjautimistiedot.
+Huomioon kannattaa, ottaa ettei tietokannanhallintajärjestelmän
+käyttäjällä ole mitään muita oikeuksia, kuin mitä se oikeasti tarvitsee.*/
 $servername = "lorem";
 $username = "ipsum";
 $password = "lorem";
--- client/ajaxTest/ajax/server.php
+++ client/ajaxTest/ajax/server.php
@@ -1,9 +1,12 @@
 <?php
+	/*Perushaku joka palauttaa koko tietokannan ristitaulukkona.*/
     if (isset($_POST['hae']) === true) {
         require('connect.php');
         $query = mysql_query("
         
         ")
+		/*Itse kysely. Tähän pitäisi vielä lisätä sanitointi
+		sekä eri kyselytyyppejä.*/
         $result = mysql_query("SELECT * FROM *;");
     }
 ?>
